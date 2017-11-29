85fa0fbbb2b6779b38fd620e691f0cab32b6faba
bjmarien <31636098+bjmarien@users.noreply.github.com>
2017-11-29 16:18:26 -0500

    final

---
+++ XMLExample1/XMLExample/src/main/java/pkgLibrary/Catalog.java
@@ -0,0 +1,61 @@
+package pkgLibrary;
+
+import java.util.ArrayList;
+
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class Catalog {
+
+	@XmlAttribute
+	int id;	
+	
+	@XmlElement(name="book")
+	ArrayList<Book> books;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<Book> getBooks() {
+		return books;
+	}
+	
+
+	public void setBooks(ArrayList<Book> books) {
+		this.books = books;
+	}
+	
+	public Book GetBook(String BookID) 
+	{
+		Book b = null;
+		//***********************//
+		return b;
+	}
+	
+	public void AddBook(Book b) throws Exception
+	{
+		for (Book bk : this.books)
+		{
+			if (bk.getId() == b.getId())
+			{
+				throw new Exception("book exists");
+			}
+		}
+		this.books.add(b);
+	}
+
+
+	
+	
+	
+	
+}
---
+++ XMLExample1/XMLExample/src/test/java/pkgEmpty/BookTest.java
@@ -0,0 +1,23 @@
+package pkgEmpty;
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import org.junit.Test;
+
+public class BookTest {
+	
+	@Test
+	public void testGetBook() 
+	{
+		
+	}
+	
+	@Test
+	public void testAddBook() 
+	{
+		
+	}
+}
